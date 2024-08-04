d9ab1fab0632d9d642e02413cfe7d8e3be59133f
gian-tiqui <michaelgiantiqui3@gmail.com>
2024-08-04 08:09:16 +0800

    hi

--- src/controllers/donut.controller.js
+++ src/controllers/donut.controller.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export const welcome = (req, res) => {
-  res.send("meow meow ni");
+  res.send("meow meow");
 };
 
 const sendErr = (res, error) => {
