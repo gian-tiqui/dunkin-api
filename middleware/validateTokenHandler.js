a4084cc1469f0d603ec8a857577586cf5909ac3e
gian-tiqui <michaelgiantiqui3@gmail.com>
2024-08-05 08:09:36 +0800

    auth finished, protecting routes next

---
+++ middleware/validateTokenHandler.js
@@ -0,0 +1,32 @@
+import asyncHandler from "express-async-handler";
+import jwt from "jsonwebtoken";
+import { config } from "dotenv";
+
+config();
+
+const jwtFunc = async (req, res, next) => {
+  let token;
+
+  let authHeader = req.headers.Authorization || req.headers.authorization;
+
+  if (authHeader && authHeader.startsWith("Bearer")) {
+    token = authHeader.split(" ")[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+        res.status(401);
+        throw new Error("User is not authorized");
+      }
+      req.user = decoded.user;
+      next();
+    });
+
+    if (!token) {
+      res.status(401).json({ message: "user is not authorized" });
+    }
+  }
+};
+
+const validateToken = asyncHandler(jwtFunc);
+
+export default validateToken;
