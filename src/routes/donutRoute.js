b1b9edbfc8878b77a48eeec6b99a4d6dc07eda84
gian-tiqui <michaelgiantiqui3@gmail.com>
2024-08-28 08:11:58 +0800

    refresh token removed

--- src/routes/donutRoute.js
+++ src/routes/donutRoute.js
@@ -8,7 +8,6 @@ import {
   updateDonut,
 } from "../controllers/donutController.js";
 import validateToken from "../../middleware/validateTokenHandler.js";
-import { API_PREFIX } from "../server.js";
 
 const storage = multer.diskStorage({
   destination: "uploads/",
